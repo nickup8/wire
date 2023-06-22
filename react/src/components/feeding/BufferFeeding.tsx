d3c6416262b67387d8fd4e1418fa3e28fa62f92d
nickup8 <nickup8@yandex.ru>
2023-06-22 18:36:17 +0300

    Созданы компоненты страниу

---
+++ react/src/components/feeding/BufferFeeding.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const BufferFeeding = () => {
+    return <div>BufferFeeding</div>;
+};
---
+++ react/src/components/warehouse/WarehouseAcceptance.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const WarehouseAcceptance = () => {
+    return <div>WarehouseAcceptance</div>;
+};
